94e9705e95ebc489669b119dbf18b600782cac91
ismael <rakotomananaismael@gmail.com>
2026-01-14 08:14:21 +0300

    débaugage des erreur produit par le fusion forcer

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 require_once '../app/Autoloader.php';
 \App\Autoloader::register();
 
-<<<<<<< HEAD
 use App\Controllers\MembresController;
 
 
@@ -19,7 +18,6 @@ switch ($action) {
         $controller->index();
         break;
 }
-=======
 use App\Models\MembreModel;
 use App\Entities\Joueur;
 use App\Entities\Staff;
@@ -28,22 +26,8 @@ try {
     $membreModel = new MembreModel();
     $membres = $membreModel->getAll();
 
-    echo "<h1>Test du Système IFA Manager</h1>";
 
-    foreach ($membres as $membre) {
-        echo "<div>";
-        echo "<strong>Nom Complet :</strong> " . $membre->getNomComplet() . "<br>";
-        echo "<strong>Description :</strong> " . $membre->getDescription() . "<br>";
-        
-        if ($membre instanceof Joueur) {
-            echo "<em>Type détecté : Objet Joueur</em>";
-        } elseif ($membre instanceof Staff) {
-            echo "<em>Type détecté : Objet Staff</em>";
-        }
-        echo "</div><hr>";
-    }
 
 } catch (Exception $e) {
     echo "Erreur lors du test : " . $e->getMessage();
 }
->>>>>>> 0447d114f566acafb7bc89d9e50632ee7660e632
